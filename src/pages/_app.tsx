f3cac5d6d500936b28cfc2c5434cc502bd2f93f6
bruno <brunofay1@hotmail.com>
2022-10-03 17:55:28 -0300

    NEW add styles in homepage and IN product component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/globalStyles'
 import { Container, Header } from '../styles/pages/app'
+import Image from 'next/future/image'
 import logo from '../assets/Logo.svg'
+
 
 globalStyles()
 function MyApp({ Component, pageProps }: AppProps) {
   return <Container>
     <Header>
-      <img src={logo.src} alt='' />
+      <Image src={logo} alt='' />
     </Header>
     <Component {...pageProps} />
   </Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
 import type { NextPage } from 'next'
+import Image from 'next/future/image'
+import { HomeContainer, Product } from '../styles/pages/home'
+import shirtOne from '../assets/shirts/1.png'
+import shirtTwo from '../assets/shirts/2.png'
+
 
 const Home: NextPage = () => {
   return (
-   <h1>Home</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={shirtOne} alt='' width={520} height={480}/>
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirtTwo} alt='' width={520} height={480}/>
+        <footer>
+          <strong>Camiseta Beyond the Limits</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
 
