4a195891a08ecfef70be875f468c8c8760485737
bruno <brunofay1@hotmail.com>
2022-10-03 15:48:27 -0300

    NEW add global styles and theme colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/globalStyles'
 
+
+globalStyles()
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
