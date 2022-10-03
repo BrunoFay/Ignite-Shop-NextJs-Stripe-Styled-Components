dd3026a7994c14963b52b15a95120e9cd8f672a6
bruno <brunofay1@hotmail.com>
2022-10-03 16:08:14 -0300

    NEW add header component in app component (layout concept)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/globalStyles'
-
+import { Container, Header } from '../styles/pages/app'
+import logo from '../assets/Logo.svg'
 
 globalStyles()
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Container>
+    <Header>
+      <img src={logo.src} alt='' />
+    </Header>
+    <Component {...pageProps} />
+  </Container>
 }
 
 export default MyApp
