a403b2c14fb9edd5fb155f86a78d6e204f5d3445
bruno <brunofay1@hotmail.com>
2022-10-05 16:35:43 -0300

    REFACT put ssg method on productPage

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,15 +1,28 @@
+import { GetStaticProps } from "next";
+import Stripe from "stripe";
+import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductInfosContainer } from "../../styles/pages/product";
+import Image from 'next/future/image'
 
-export default function Product() {
+interface ProductProps {
+  product: {
+    id: any
+    name: string
+    price: number
+    imageURL: string
+  }
+}
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
 
       <ImageContainer>
+        <Image src={product.imageURL} alt='' width={520} height={480} />
       </ImageContainer>
 
       <ProductInfosContainer>
-        <h1>aaaa</h1>
-        <span>R$ 23212</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dicta voluptatibus reiciendis pariatur praesentium quis error. Nobis impedit in veniam quod, obcaecati dolore maxime perferendis doloremque recusandae iusto quas esse!
         </p>
@@ -18,4 +31,27 @@ export default function Product() {
       </ProductInfosContainer>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params['id'];
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+  const price = product.default_price as Stripe.Price
+  const priceFormatted = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(price.unit_amount! / 100)
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageURL: product.images[0],
+        price: priceFormatted
+      }
+    }
+  }
 }
