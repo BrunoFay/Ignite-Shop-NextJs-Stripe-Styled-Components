0b731de57a3f82aa51e2d8f20a6542063bb5e5f8
bruno <brunofay1@hotmail.com>
2022-10-05 15:48:45 -0300

    NEW add product page and css

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,21 @@
+import { ImageContainer, ProductContainer, ProductInfosContainer } from "../../styles/pages/product";
+
+export default function Product() {
+  return (
+    <ProductContainer>
+
+      <ImageContainer>
+      </ImageContainer>
+
+      <ProductInfosContainer>
+        <h1>aaaa</h1>
+        <span>R$ 23212</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dicta voluptatibus reiciendis pariatur praesentium quis error. Nobis impedit in veniam quod, obcaecati dolore maxime perferendis doloremque recusandae iusto quas esse!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductInfosContainer>
+    </ProductContainer>
+  )
+}
